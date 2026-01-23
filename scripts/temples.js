c784010183070f9693cc7c95a315026fc0c0c96f
Edwell22 <113574992+Edwell22@users.noreply.github.com>
2026-01-23 20:04:47 +0200

    new

--- scripts/temples.js
+++ scripts/temples.js
@@ -66,3 +66,86 @@ const temples = [
 	},
 	// Add more temple objects here...
   ];
+
+
+const homeLink = document.getElementById("home-link");
+const oldLink = document.getElementById("old-link");
+const newLink = document.getElementById("new-link");
+const largeLink = document.getElementById("large-link");
+const smallLink = document.getElementById("small-link");
+const heading = document.querySelector(" h3");
+
+createTemple(temples);
+
+// this code is  refering to a actual info when user click  the link
+homeLink.addEventListener("click", () => {
+  createTemple(temples);
+  heading.textContent = " Home ";
+});
+
+newLink.addEventListener("click", () => {
+  const newTemples = temples.filter((temple) => {
+    const year = new Date(temple.dedicated).getFullYear();
+    return year >= 2005 && year <= 2024;
+  });
+  createTemple(newTemples);
+  heading.textContent = "New"
+});
+
+oldLink.addEventListener("click", () => {
+	const newTemples = temples.filter((temple) => {
+	  const year = new Date(temple.dedicated).getFullYear();
+	  return  year < 2005;
+	});
+	createTemple(newTemples);
+	heading.textContent = "Old"
+  });
+
+largeLink.addEventListener("click", () => {
+  const largeTemples = temples.filter((temple) => temple.area >= 10000);
+  createTemple(largeTemples);
+  heading.textContent = "Large"
+});
+
+smallLink.addEventListener("click", () => {
+  const smallTemples = temples.filter((temple) => temple.area < 10000);
+  createTemple(smallTemples);
+  heading.textContent = "Small"
+});
+ 
+// end of code 
+
+
+
+//this code  will display info about temples 
+  function createTemple(ftemble) {
+	document.querySelector(".res-grid").innerHTML ="";
+	ftemble.forEach(temple =>{
+		let card = document.createElement("section");
+		let name = document.createElement("h3");
+		let location = document.createElement("p");	
+		let dedicated = document.createElement("p");
+		let area = document.createElement("p");
+		let image = document.createElement("img");
+
+		name.textContent = temple.templeName
+
+		location.innerHTML = `<span class="label">Location:</span> ${temple.location}`;
+		dedicated.innerHTML = `<span class="label">Dedicated:</span> ${temple.dedicated}`;
+		area.innerHTML = `<span class="label">Area:</span> ${temple.area} sq. ft.`;
+		image.setAttribute("src" ,temple.imageUrl);
+		image.setAttribute("alt" ,`${temple.templeName} temple`);
+		image.setAttribute("loading", "lazy");
+
+		card.appendChild(name);
+		card.appendChild(location);
+		card.appendChild(dedicated);
+		card.appendChild(area);
+		card.appendChild(image);
+
+		document.querySelector(".res-grid").appendChild(card);
+
+		
+	})
+	
+  }
